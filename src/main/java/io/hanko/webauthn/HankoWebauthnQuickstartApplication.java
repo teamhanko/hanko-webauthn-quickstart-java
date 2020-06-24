d9600a27e9db40d733b7a4270596bf0107b2d0e8
Lennart Fleischmann <lennart.fleischmann@hanko.io>
2020-06-24 13:13:59 +0200

    Issue #33 Initialize project

---
+++ src/main/java/io/hanko/webauthn/HankoWebauthnQuickstartApplication.java
@@ -0,0 +1,13 @@
+package io.hanko.webauthn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HankoWebauthnQuickstartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HankoWebauthnQuickstartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/hanko/webauthn/HankoWebauthnQuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hanko.webauthn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HankoWebauthnQuickstartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
